860741d27e566d1301f6525310f20b6976aae958
Ryan Mui <reachryanmui@gmail.com>
2019-03-30 19:46:46 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,16 +1,16 @@
-INSERT INTO bears (name, gender, color, temperament, alive)
-    VALUES ("Mr. Chocolate", "Male", "Brown", "Friendly", 1);
-INSERT INTO bears (name, gender, color, temperament, alive)
-    VALUES ("Rowdy", "Female", "Red", "Friendly", 1);
-INSERT INTO bears (name, gender, color, temperament, alive)
-    VALUES ("Tabitha", "Female", "White", "Hostile", 1);
-INSERT INTO bears (name, gender, color, temperament, alive)
-    VALUES ("Sergeant Brown", "Male", "Brown", "Stern", 1);
-INSERT INTO bears (name, gender, color, temperament, alive)
-    VALUES ("Melissa", "Female", "Red", "Peppy", 0);
-INSERT INTO bears (name, gender, color, temperament, alive)
-    VALUES ("Grinch", "Male", "Green", "Hostile", 0);
-INSERT INTO bears (name, gender, color, temperament, alive)
-    VALUES ("Wendy", "Female", "Black", "Hostile", 1);
-INSERT INTO bears (name, gender, color, temperament, alive)
-    VALUES (NULL, "Male", "Black", "Quiet", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ("Mr. Chocolate", 17, "Male", "Brown", "Friendly", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ("Rowdy", 10,"Female", "Red", "Friendly", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ("Tabitha", 4, "Female", "White", "Hostile", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ("Sergeant Brown", 57, "Male", "Brown", "Stern", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ("Melissa", 23, "Female", "Red", "Peppy", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ("Grinch", 3, "Male", "Green", "Hostile", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ("Wendy", 10, "Female", "Black", "Hostile", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES (NULL, 15, "Male", "Black", "Quiet", 1);
