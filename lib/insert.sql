ef10718ed2f25dfe41051fd8db0a8dcf7d678be3
Ryan Mui <reachryanmui@gmail.com>
2019-03-30 19:43:11 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,18 +1,18 @@
 INSERT INTO bears (name, gender, color, temperament, alive)
     VALUES ("John", "Male", "White", "Hostile", 1);
 INSERT INTO bears (name, gender, color, temperament, alive)
-    VALUES ("Mr. Chocolate", "Male", "Brown", "Hostile", 1);
+    VALUES ("Mr. Chocolate", "Male", "Brown", "Friendly", 1);
 INSERT INTO bears (name, gender, color, temperament, alive)
-    VALUES ("Rowdy", "Female", "Red", "Hostile", 1);
+    VALUES ("Rowdy", "Female", "Red", "Friendly", 1);
 INSERT INTO bears (name, gender, color, temperament, alive)
     VALUES ("Tabitha", "Female", "White", "Hostile", 1);
 INSERT INTO bears (name, gender, color, temperament, alive)
-    VALUES ("Sergeant Brown", "Male", "Brown", "Hostile", 1);
+    VALUES ("Sergeant Brown", "Male", "Brown", "Stern", 1);
 INSERT INTO bears (name, gender, color, temperament, alive)
-    VALUES ("Melissa", "Female", "Red", "Hostile", 1);
+    VALUES ("Melissa", "Female", "Red", "Peppy", 1);
 INSERT INTO bears (name, gender, color, temperament, alive)
     VALUES ("Grinch", "Male", "Green", "Hostile", 1);
 INSERT INTO bears (name, gender, color, temperament, alive)
     VALUES ("Wendy", "Female", "Black", "Hostile", 1);
 INSERT INTO bears (name, gender, color, temperament, alive)
-    VALUES (?, "Male", "Black", "Hostile", 1);
+    VALUES (?, "Male", "Black", "Quiet", 1);
