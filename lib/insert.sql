32bffe452bd0a97d936cab301291bb34fb32e69e
Ryan Mui <reachryanmui@gmail.com>
2019-03-30 19:41:43 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,3 @@
-gender TEXT,
-color	TEXT,
-temperament	TEXT,
-alive	IN
-
 INSERT INTO bears (name, gender, color, temperament, alive)
     VALUES ("John", "Male", "White", "Hostile", 1);
 INSERT INTO bears (name, gender, color, temperament, alive)
