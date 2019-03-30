bbe27c5da90f1417448162a94c39a948910a04b5
Ryan Mui <reachryanmui@gmail.com>
2019-03-30 19:39:22 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -5,3 +5,12 @@ alive	IN
 
 INSERT INTO bears (name, gender, color, temperament, alive)
     VALUES ("John", "Male", "White", "Hostile", 1);
+INSERT INTO bears (name, gender, color, temperament, alive)
+    VALUES ("John", "Male", "White", "Hostile", 1);
+    Mr. Chocolate
+    Rowdy
+    Tabitha
+    Sergeant Brown
+    Melissa
+    Grinch
+    Wendy
