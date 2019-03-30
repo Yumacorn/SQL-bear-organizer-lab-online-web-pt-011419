0d425e1d762318494cfecb0a3f3f1a42d8e61e1b
Ryan Mui <reachryanmui@gmail.com>
2019-03-30 19:42:35 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,9 +1,9 @@
 INSERT INTO bears (name, gender, color, temperament, alive)
     VALUES ("John", "Male", "White", "Hostile", 1);
 INSERT INTO bears (name, gender, color, temperament, alive)
-    VALUES ("Mr. Chocolate", "Male", "White", "Hostile", 1);
+    VALUES ("Mr. Chocolate", "Male", "Brown", "Hostile", 1);
 INSERT INTO bears (name, gender, color, temperament, alive)
-    VALUES ("Rowdy", "Female", "White", "Hostile", 1);
+    VALUES ("Rowdy", "Female", "Red", "Hostile", 1);
 INSERT INTO bears (name, gender, color, temperament, alive)
     VALUES ("Tabitha", "Female", "White", "Hostile", 1);
 INSERT INTO bears (name, gender, color, temperament, alive)
@@ -13,6 +13,6 @@ INSERT INTO bears (name, gender, color, temperament, alive)
 INSERT INTO bears (name, gender, color, temperament, alive)
     VALUES ("Grinch", "Male", "Green", "Hostile", 1);
 INSERT INTO bears (name, gender, color, temperament, alive)
-    VALUES ("Wendy", "Female", "White", "Hostile", 1);
+    VALUES ("Wendy", "Female", "Black", "Hostile", 1);
 INSERT INTO bears (name, gender, color, temperament, alive)
-    VALUES (?, "Male", "White", "Hostile", 1);
+    VALUES (?, "Male", "Black", "Hostile", 1);
